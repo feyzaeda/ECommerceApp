df6c377e83bd7cb4aaf9c8b2b837bd8d3ecfc28c
feyzaeda <fturk17@posta.pau.edu.tr>
2021-10-13 17:17:26 +0300

    added navigation and bottom navigation bar

--- app/src/main/java/com/e_commerceapp/android/MainActivity.kt
+++ app/src/main/java/com/e_commerceapp/android/MainActivity.kt
@@ -2,10 +2,30 @@ package com.e_commerceapp.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.e_commerceapp.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUi()
+    }
+
+    private fun initUi() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navigationHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigationView,
+            navHostFragment.navController
+        )
     }
 }
