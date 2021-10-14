b4b5eb343f24785b8606c656466b36c6ac8a7981
feyzaeda <fturk17@posta.pau.edu.tr>
2021-10-14 19:35:04 +0300

    created add image button

--- app/src/main/java/com/e_commerceapp/android/AddProductFragment.kt
+++ app/src/main/java/com/e_commerceapp/android/AddProductFragment.kt
@@ -1,12 +1,19 @@
 package com.e_commerceapp.android
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.app.ActivityCompat
 import com.e_commerceapp.android.databinding.FragmentAddProductBinding
+import java.util.*
 
 class AddProductFragment : Fragment() {
 
@@ -29,9 +36,35 @@ class AddProductFragment : Fragment() {
     private fun initUi() {
         binding.apply {
             val category = resources.getStringArray(R.array.category)
-            val arrayAdapter = ArrayAdapter(requireContext(),R.layout.dropdown_category_item,category)
+            val arrayAdapter = ArrayAdapter(requireContext(),R.layout.item_dropdown_category,category)
             binding.autoCompleteCategory.setAdapter(arrayAdapter)
+            if (ActivityCompat.checkSelfPermission(binding.root.context, android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.CAMERA),111)
+            }
+
+            binding.btnAddProductImg.setOnClickListener {
+                var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent,101)
+            }
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==101){
+            var picture = data?.getParcelableExtra<Bitmap>("data")
+            binding.imgProduct.setImageBitmap(picture)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            binding.btnAddProductImg.isEnabled = true
+        }
+    }
 }
