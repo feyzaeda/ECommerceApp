84218940ad7e1c23bc0ef6a10eb479338184c0e4
feyzaeda <fturk17@posta.pau.edu.tr>
2021-10-23 02:30:45 +0300

    refactor product data class

--- app/src/main/java/com/e_commerceapp/android/Product.kt
+++ app/src/main/java/com/e_commerceapp/android/Product.kt
@@ -1,3 +1,10 @@
 package com.e_commerceapp.android
 
-data class Product(var productName : String ?= null, var productExplanation : String ?= null, var productCategory : String ?= null, var productPrice : String ?= null, var productId : String ?= null, var productImg : String ?= null)
+data class Product(
+    var productName: String? = null,
+    var productExplanation: String? = null,
+    var productCategory: String? = null,
+    var productPrice: String? = null,
+    var productId: String? = null,
+    var productImg: String? = null
+)
