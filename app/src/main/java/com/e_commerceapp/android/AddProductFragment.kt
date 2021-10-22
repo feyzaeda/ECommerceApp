90bd2b662ea2ba46a489c87e1a9c0245a0d57391
feyzaeda <fturk17@posta.pau.edu.tr>
2021-10-23 01:33:50 +0300

    add product fragment refactor

--- app/src/main/java/com/e_commerceapp/android/AddProductFragment.kt
+++ app/src/main/java/com/e_commerceapp/android/AddProductFragment.kt
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.e_commerceapp.android.DatabaseConstants.PRODUCT_DB
 import com.e_commerceapp.android.databinding.FragmentAddProductBinding
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
@@ -20,15 +21,11 @@ class AddProductFragment : Fragment() {
     private val binding get() = _binding!!
     private var imgUri: Uri? = null
     private var productCategory: String? = null
-    private var productName: String? = null
-    private var productExplanetion: String? = null
-    private var productPrice: String? = null
-    private var strUri: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentAddProductBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -36,74 +33,67 @@ class AddProductFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initUi()
+        setAutoComplete()
     }
 
     private fun initUi() {
         binding.apply {
-            val category = resources.getStringArray(R.array.category)
-            val arrayAdapter =
-                ArrayAdapter(requireContext(), R.layout.item_dropdown_category, category)
-            autoCompleteCategory.setAdapter(arrayAdapter)
-
-            autoCompleteCategory.setOnItemClickListener { parent, view, position, id ->
-                productCategory = parent.getItemAtPosition(position).toString()
-            }
-
-            btnAddProductImg.setOnClickListener {
-                val intent = Intent()
-                intent.type = "image/"
-                intent.action = Intent.ACTION_GET_CONTENT
-                startActivityForResult(intent, 100)
-            }
-
-            btnAddProduct.setOnClickListener {
-                val database = FirebaseDatabase.getInstance().getReference("Product")
-                val productId = database.push().key.toString()
-
-
-                if (productCategory.isNullOrEmpty().not() &&
-                    txtProductName.text.isNullOrEmpty().not() &&
-                    txtProductExplanation.text.isNullOrEmpty().not() &&
-                    txtProductPrice.text.isNullOrEmpty().not() &&
-                    imgUri.toString().isEmpty().not()
-                ) {
-                    val storage =
-                        FirebaseStorage.getInstance().getReference("Product").child(productId)
-                    storage.putFile(imgUri!!).addOnSuccessListener {
-                        storage.downloadUrl.addOnSuccessListener {
-                            strUri = it.toString()
-                            productName = txtProductName.text.toString()
-                            productExplanetion = txtProductExplanation.text.toString()
-                            productPrice = txtProductPrice.text.toString()
-                            val product = Product(
-                                productName,
-                                productExplanetion,
-                                productCategory,
-                                productPrice,
-                                productId,
-                                strUri
-                            )
-                            database.child(productCategory!!).child(productId)
-                                .setValue(product)
-
-                            Navigation.findNavController(binding.root)
-                                .navigate(R.id.action_addProductFragment_to_homeFragment)
-                        }
-                    }
-
-                } else {
-                    Toast.makeText(
-                        binding.root.context,
-                        "Lütfen tüm alanları doldurunuz.",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            }
-
-
+            btnAddProductImg.setOnClickListener { selectImageFromFiles() }
+            btnAddProduct.setOnClickListener { addProduct() }
         }
     }
 
+    private fun addProduct() {
+        val database = FirebaseDatabase.getInstance().getReference("Product")
+        val productId = database.push().key.toString()
+        val productName = binding.txtProductName.text.toString()
+        val productExplanation = binding.txtProductExplanation.text.toString()
+        val productPrice = binding.txtProductPrice.text.toString()
+
+        if (productCategory.isNullOrEmpty().not() &&
+            productName.isEmpty().not() &&
+            productExplanation.isEmpty().not() &&
+            productPrice.isEmpty().not() &&
+            imgUri.toString().isEmpty().not()
+        ) {
+            val storage = FirebaseStorage.getInstance().getReference(PRODUCT_DB).child(productId)
+            storage.putFile(imgUri!!).addOnSuccessListener {
+                storage.downloadUrl.addOnSuccessListener {
+                    val product = Product(
+                        productName,
+                        productExplanation,
+                        productCategory,
+                        productPrice,
+                        productId,
+                        it.toString()
+                    )
+                    database.child(productCategory!!).child(productId).setValue(product)
+                        .addOnSuccessListener {
+                            Navigation.findNavController(binding.root)
+                                .navigate(R.id.action_addProductFragment_to_homeFragment)
+                        }
+                }
+            }
+        } else {
+            Toast.makeText(requireContext(), R.string.empty_fields_error, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun selectImageFromFiles() {
+        val intent = Intent()
+        intent.type = "image/"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, 100)
+    }
+
+    private fun setAutoComplete() {
+        val category = resources.getStringArray(R.array.category)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.item_dropdown_category, category)
+        binding.autoCompleteCategory.setAdapter(arrayAdapter)
+        binding.autoCompleteCategory.setOnItemClickListener { parent, view, position, id ->
+            productCategory = parent.getItemAtPosition(position).toString()
+        }
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -113,5 +103,9 @@ class AddProductFragment : Fragment() {
         }
     }
 
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 
 }
---
+++ app/src/main/java/com/e_commerceapp/android/DatabaseConstants.kt
@@ -0,0 +1,5 @@
+package com.e_commerceapp.android
+
+object DatabaseConstants {
+    const val PRODUCT_DB = "Product"
+}
