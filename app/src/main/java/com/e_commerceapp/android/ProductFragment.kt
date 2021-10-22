e4aa5b7525496a0b46872b6b1b57c3c4ae4da601
feyzaeda <fturk17@posta.pau.edu.tr>
2021-10-23 02:10:36 +0300

    product fragment refactor

--- app/src/main/java/com/e_commerceapp/android/ProductFragment.kt
+++ app/src/main/java/com/e_commerceapp/android/ProductFragment.kt
@@ -1,87 +1,93 @@
 package com.e_commerceapp.android
 
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
+import com.e_commerceapp.android.DatabaseConstants.PRODUCT_DB
 import com.e_commerceapp.android.databinding.FragmentProductBinding
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.storage.FirebaseStorage
-import kotlinx.android.synthetic.main.fragment_add_product.*
-import kotlinx.android.synthetic.main.item_product.*
-import java.io.File
+import com.google.firebase.database.*
 
-class ProductFragment : Fragment() {
+class ProductFragment : Fragment(), ProductAdapter.OnItemClickListener {
 
     private var _binding: FragmentProductBinding? = null
     private val binding get() = _binding!!
-    private lateinit var productList: ArrayList<Product>
+    private var productList: ArrayList<Product> = arrayListOf()
+    private lateinit var adapter: ProductAdapter
+    private var productCategory: String? = null
+    private lateinit var database: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentProductBinding.inflate(inflater, container, false)
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        getParams()
+        initDatabase()
         initUi()
+        getProducts()
+    }
+
+    private fun getParams() {
+        val args: HomeFragmentArgs by navArgs()
+        productCategory = args.productCategory
+    }
+
+    private fun initDatabase() {
+        database = FirebaseDatabase.getInstance().getReference(PRODUCT_DB)
+            .child(productCategory!!)
     }
 
     private fun initUi() {
+        adapter = ProductAdapter(productList, this)
         binding.apply {
-            val args: HomeFragmentArgs by navArgs()
-            val productCategory = args.productCategory
+            rcyProduct.adapter = adapter
             rcyProduct.layoutManager = GridLayoutManager(binding.root.context, 2)
             rcyProduct.setHasFixedSize(true)
-            productList = arrayListOf<Product>()
-            var database =
-                FirebaseDatabase.getInstance().getReference("Product").child(productCategory)
-            database.addValueEventListener(object : ValueEventListener,
-                ProductAdapter.onItemClickListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (snapshot.exists()) {
-                        productList.clear()
-                        for (productSnapShot in snapshot.children) {
-                            val product = productSnapShot.getValue(Product::class.java)
-                            productList.add(product!!)
-                        }
-                        productList.sortByDescending {
-                            it.productName
-                        }
-                        var adapter = ProductAdapter(productList, this)
-                        rcyProduct.adapter = adapter
-                    }
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun onItemClick(categoryName: String?, productId: String?) {
-                    if (productId.isNullOrEmpty().not() && categoryName.isNullOrEmpty().not()){
-                        val action = ProductFragmentDirections.actionProductFragmentToProductDetailFragment(productId!!,categoryName!!)
-                        findNavController().navigate(action)
-                    }
-                }
-
-            })
-
-
         }
     }
 
+    private fun getProducts(){
+        database.child(productCategory!!).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    productList.clear()
+                    for (productSnapShot in snapshot.children) {
+                        val product = productSnapShot.getValue(Product::class.java)
+                        productList.add(product!!)
+                    }
+                    productList.sortByDescending {
+                        it.productName
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+    }
 
+    override fun onItemClick(categoryName: String?, productId: String?) {
+        if (productId.isNullOrEmpty().not() && categoryName.isNullOrEmpty().not()) {
+            val action = ProductFragmentDirections.actionProductFragmentToProductDetailFragment(
+                productId!!,
+                categoryName!!
+            )
+            findNavController().navigate(action)
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 }
